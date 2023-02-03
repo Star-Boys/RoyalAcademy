c0b1f34cb62702642fe86427aab6603d7393e988
anish <anishbista2056@gmail.com>
2023-02-03 21:37:30 +0545

    first commit

---
+++ royalc_academy_api/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class RoyalcAcademyApiConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'royalc_academy_api'
---
+++ royalc_academy_api/views/views_login.py
@@ -0,0 +1,10 @@
+from rest_framework.decorators import APIView
+
+
+class LoginApiView(APIView):
+    authentication_classes = []
+    permission_classes = []
+
+    def post(self, request):
+        pass
+
